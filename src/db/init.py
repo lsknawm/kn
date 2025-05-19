99c456d5d377a8c3bc86259481bf190d07eff1cf
lsknawm <lsknawm@gmail.com>
2025-05-19 17:42:00 +0800

    简单项目搭建

--- src/db/init.py
+++ src/db/init.py
@@ -1,11 +1,2 @@
 # 定义一个数据库连接函数，我用的是mysql
 # 编码为utf8mb4，ip为192.168.10.100，密码和账号为root
-def get_db():
-    import pymysql
-    db = pymysql.connect(host='192.168.10.100',
-                         user='root',
-                         password='root',
-                         db='KN',
-                         charset='utf8mb4',
-                         cursorclass=pymysql.cursors.DictCursor)
-    return db
